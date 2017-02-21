58a8a8c32cd0e9283ffa3159c57d9d852c1b2a5b
Francois Galtie <francois.galtie@gmail.com>
2017-02-21 15:09:55 +0100

    seed ok, without images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'products.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  Product.create!(row.to_hash)
+end
+
+# csv.each do |row|
+#   Product.create! \
+#     name: row['name']
+#     brand: row['brand']
+#     image: row['image']
+#     price: row['price']
+#     description: row['description'
+#     description_short: row['description_short']
+#     gender: row['gender']
+#     tag_one: row['tag_one']
+#     tag_two: row['tag_two']
+#     sell_priority: row['sell_priority']
+#     supplier_name: ow['supplier_name']
+#     stock: row['stock']
+#     availability: row['availability']
+#     online_supplied: row['online_supplied']
+# end
