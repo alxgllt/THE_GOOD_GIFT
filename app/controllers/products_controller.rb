429b6dc2e5d5cd7283082a1f8805648b1616c0b5
Eytan Messika <eytan.messika@edhec.com>
2017-02-21 13:10:21 +0100

    added flow and routes that link the pages + crud for orders in ordercontrollers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
+
+  def index
+    @products = Product.all
+    # business intelligence
+    @order = Order.new()
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :products
   resources :orders
+
+  #Selecttag page flow n°2
+  get '/select_tags', to: 'pages#select_tags'
+
 
 end
