9c6a6abf42119d8dc433c0c0a026b8a61445ea96
alxgllt <alexandreguillot89@gmail.com>
2017-02-21 12:48:47 +0100

    almost done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
+require "yaml"
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
   end
+
+tags_as_hashes = YAML.load_file(File.join(File.dirname(__FILE__), "../../db/tags.yml"))
+@tags_as_objects = tags_as_hashes.map { |tag| Tag.new(tag.symbolize_keys) }
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -8,10 +8,4 @@ class Tag
     @image_file_name = attributes[:image_file_name]
   end
 
-# # to load the yaml (you get an array of hashes):
-# tags_as_hashes = YAML.load_file(File.join(File.dirname(__FILE__), "tags.yml"))
-
-# # to have an array of Tag objects:
-# @tags_as_objects = tags_as_hashes.map { |tag| Tag.new(tag) }
-
 end
