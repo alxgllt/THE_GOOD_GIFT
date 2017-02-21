6a72ff5528324fbe8875cd2d3afb8a35f9a18191
alxgllt <alexandreguillot89@gmail.com>
2017-02-21 18:30:33 +0100

    working

--- app/assets/javascripts/init_datepicker.js
+++ app/assets/javascripts/init_datepicker.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $('.datepicker').datepicker({
-    format: "dd/mm/yyyy",
+  $('#datepicker').datepicker({
+    format: "dd-mm-yyyy",
     language: "fr",
     daysOfWeekDisabled: "0,6",
     autoclose: true
