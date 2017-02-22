6b577cb746e8ce46740a9b8e5277398c71d50842
Gauthier Levha <levha.g@gmail.com>
2017-02-22 18:43:13 +0100

    algo v1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,6 +48,15 @@ class ProductsController < ApplicationController
   end
 
   private
+
+  def algo_matching(products, price)
+    product_list = list_format(products)
+    available_cash = calc_available_cash(price)
+    default_bundle_config = default_bundle_configuration(available_cash)
+    matching_list = price_matching(available_cash, default_bundle_config, product_list)
+    sorting_matching_list(matching_list)
+  end
+
   def list_format(products)
     product_list = {
       big: [],
@@ -76,12 +85,12 @@ class ProductsController < ApplicationController
       medium: 0,
       small: 0,
       proportion: {
-        big_proportion: 0.5,
-        medium_proportion: 0.3,
-        small_proportion: 0.2
+        big_proportion: 0,
+        medium_proportion: 0,
+        small_proportion: 0
       }
     }
-    if available_cash <= 100 && available_cash < 150
+    if available_cash >= 85 && available_cash < 150
       default_bundle_config[:big] = 0
       default_bundle_config[:medium] = 1
       default_bundle_config[:small] = 1
@@ -95,27 +104,41 @@ class ProductsController < ApplicationController
       default_bundle_config[:proportion][:big_proportion] = 0
       default_bundle_config[:proportion][:medium_proportion] = 0.4
       default_bundle_config[:proportion][:small_proportion] = 0.2
-    elsif available_cash <= 250 && available_cash < 400
+    elsif available_cash <= 250 && available_cash < 325
+      default_bundle_config[:big] = 1
+      default_bundle_config[:medium] = 1
+      default_bundle_config[:small] = 1
+      default_bundle_config[:proportion][:big_proportion] = 0.6
+      default_bundle_config[:proportion][:medium_proportion] = 0.275
+      default_bundle_config[:proportion][:small_proportion] = 0.125
+    elsif available_cash <= 325 && available_cash < 400
       default_bundle_config[:big] = 1
       default_bundle_config[:medium] = 1
       default_bundle_config[:small] = 1
       default_bundle_config[:proportion][:big_proportion] = 0.5
+      default_bundle_config[:proportion][:medium_proportion] = 0.375
+      default_bundle_config[:proportion][:small_proportion] = 0.125
+    elsif available_cash <= 400 && available_cash < 500
+      default_bundle_config[:big] = 1
+      default_bundle_config[:medium] = 1
+      default_bundle_config[:small] = 2
+      default_bundle_config[:proportion][:big_proportion] = 0.5
       default_bundle_config[:proportion][:medium_proportion] = 0.3
-      default_bundle_config[:proportion][:small_proportion] = 0.2
-    elsif available_cash <= 400 && available_cash < 600
+      default_bundle_config[:proportion][:small_proportion] = 0.1
+    elsif available_cash <= 500 && available_cash < 600
       default_bundle_config[:big] = 1
       default_bundle_config[:medium] = 1
       default_bundle_config[:small] = 1
-      default_bundle_config[:proportion][:big_proportion] = 0.5
-      default_bundle_config[:proportion][:medium_proportion] = 0.3
-      default_bundle_config[:proportion][:small_proportion] = 0.2
-     else
+      default_bundle_config[:proportion][:big_proportion] = 0.667
+      default_bundle_config[:proportion][:medium_proportion] = 0.25
+      default_bundle_config[:proportion][:small_proportion] = 0.083
+    else
       default_bundle_config[:big] = 2
       default_bundle_config[:medium] = 1
       default_bundle_config[:small] = 1
-      default_bundle_config[:proportion][:big_proportion] = 0.3
+      default_bundle_config[:proportion][:big_proportion] = 0.345
       default_bundle_config[:proportion][:medium_proportion] = 0.25
-      default_bundle_config[:proportion][:small_proportion] = 0.15
+      default_bundle_config[:proportion][:small_proportion] = 0.06
     end
   end
 
@@ -125,7 +148,43 @@ class ProductsController < ApplicationController
       medium: [],
       small: []
     }
-    product_list
+    # si pas de big dans config, on supprime tout:
+    unless default_bundle_config[:big] == 0
+      # on itère sur les big
+      product_list[:big].each do |product|
+        if product.price <= available_cash * default_bundle_config[:proportion][:big_proportion] &&
+            product.price >= available_cash * default_bundle_config[:proportion][:big_proportion] * 0.85
+          matching_list[:big] << product
+        end
+      end
+    end
+    # si pas de medium dans config, on supprime tout:
+    unless default_bundle_config[:medium] == 0
+      # on itère sur les medium
+      product_list[:medium].each do |product|
+        if product.price <= available_cash * default_bundle_config[:proportion][:medium_proportion] &&
+            product.price >= available_cash * default_bundle_config[:proportion][:medium_proportion] * 0.85
+          matching_list[:medium] << product
+        end
+      end
+    end
+    # si pas de small dans config, on supprime tout:
+    unless default_bundle_config[:small] == 0
+      # on itère sur les small
+      product_list[:small].each do |product|
+        if product.price <= available_cash * default_bundle_config[:proportion][:small_proportion] &&
+            product.price >= available_cash * default_bundle_config[:proportion][:small_proportion] * 0.75
+          matching_list[:small] << product
+        end
+      end
+    end
+    return matching_list
+  end
+
+  def sorting_matching_list(matching_list)
+    matching_list[:big].sort_by! { |product| product[:sell_priority] }
+    matching_list[:medium].sort_by! { |product| product[:sell_priority] }
+    matching_list[:small].sort_by! { |product| product[:sell_priority] }
   end
 
 end
