09ba395e84322767e0c2dbac8caa8d79583da5e4
Eytan Messika <eytan.messika@edhec.com>
2017-02-23 19:14:01 +0100

    s

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -8,3 +8,18 @@ function initCarousel() {
     navText: ["Précedent","Suivant"],
   });
 }
+
+function Ordercount () {
+  owly.on('changed.owl.carousel', function(event) {
+        $(".order-button").attr("disabled", true);
+        setTimeout(function() {
+          var product_1 = $('.owl-carousel.owl-big .owl-item.active').children().data('id');
+          var product_2 = $('.owl-carousel.owl-medium .owl-item.active').children().data('id');
+          var product_3 = $('.owl-carousel.owl-small .owl-item.active').children().data('id');
+          $("#big").val(product_1);
+          $("#medium").val(product_2);
+          $("#small").val(product_3);
+          $(".order-button").removeAttr("disabled");
+        }, 800);
+      });
+}
