a5849eb6de4f7603abefcd63237e84f7718237c6
Francois Galtie <francois.galtie@gmail.com>
2017-02-27 16:51:54 +0100

    added phone number to model and form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
+
+  validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Format email non valide." }, allow_nil: true
+  validates :phone, format: { with: /\b\d{3}[-.]?\d{3}[-.]?\d{4}\b/, message: "Format de téléphone non valide." }, allow_nil: true
 end
---
+++ db/migrate/20170227125958_add_phone_to_orders.rb
@@ -0,0 +1,5 @@
+class AddPhoneToOrders < ActiveRecord::Migration[5.0]
+  def change
+    add_column :orders, :phone, :string
+  end
+end
