84181854702dc9e2b4af50522a4686b60d572ff6
Gauthier Levha <levha.g@gmail.com>
2017-02-28 16:42:56 +0100

    algo v1 ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227125958) do
+ActiveRecord::Schema.define(version: 20170228090534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,8 @@ ActiveRecord::Schema.define(version: 20170227125958) do
     t.string   "email"
     t.integer  "cost"
     t.string   "phone"
-    t.string   "company"
     t.integer  "total_price_cents", default: 0, null: false
+    t.string   "company"
   end
 
   create_table "product_lists", force: :cascade do |t|
